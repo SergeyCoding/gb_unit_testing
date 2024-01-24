72394d26ff732c4f0705b4c9ff337440b840296d
SergeyXP <s.pusthotin@gmail.com>
2024-01-24 19:55:04 +0400

    hw01

---
+++ hw01/src/test/java/com/gb/unit/testing/calculator/CalculatorTest.java
@@ -0,0 +1,22 @@
+package com.gb.unit.testing.calculator;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void calculation() {
+    }
+}
