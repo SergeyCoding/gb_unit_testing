8ab3558c1c1af8f109e3e4336dc3e0ff71835997
SergeyXP <s.pusthotin@gmail.com>
2024-02-03 15:17:40 +0400

    hw04

--- hw04/src/test/java/seminars/fourth/book/BookServiceTest.java
+++ hw04/src/test/java/seminars/fourth/book/BookServiceTest.java
@@ -1,22 +1,41 @@
 package seminars.fourth.book;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BookServiceTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void findBookById() {
+        var mockBookService = Mockito.mock(BookRepository.class);
+        Mockito.when(mockBookService.findById("5")).thenReturn(new Book("5"));
+
+        var bs = new BookService(mockBookService);
+        Book bookById = bs.findBookById("5");
+
+        assertThat(bookById.getId()).isEqualTo("5");
     }
 
     @org.junit.jupiter.api.Test
     void findAllBooks() {
+        var mockBookService = Mockito.mock(BookRepository.class);
+
+        var mockBooks = new ArrayList<Book>();
+
+        for (int i = 0; i < 5; i++) {
+            mockBooks.add(new Book(Integer.toString(i)));
+        }
+        Mockito.when(mockBookService.findAll()).thenReturn(mockBooks);
+
+        var bs = new BookService(mockBookService);
+
+        var books = bs.findAllBooks();
+
+        assertThat(books.size()).isEqualTo(5);
+        assertThat(books.stream().map(Book::getId).toArray()).containsExactly("0", "1", "2", "3", "4");
     }
 }
