50a4eead0a1052b98b0e235d73fa09172fe92307
SergeyXP <s.pusthotin@gmail.com>
2024-02-03 14:00:36 +0400

    hw04

---
+++ hw04/src/test/java/seminars/fourth/book/BookServiceTest.java
@@ -0,0 +1,22 @@
+package seminars.fourth.book;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findBookById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findAllBooks() {
+    }
+}
